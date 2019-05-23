e9bfff2fc4db15a084793d552ab95674cf0fc403
s.kizilkurt <s.kizilkurt@triodor.eu>
2019-05-23 10:11:14 +0300

    switch to web client view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -153,7 +153,7 @@ const appRoutes: Routes =
       { provide: APP_INITIALIZER, useFactory: ConfigLoader, deps: [ConfigService], multi: true }
   ]
   ,
-  bootstrap: [DynamicsTestComponent]
+  bootstrap: [AppEntryComponent]
 })
 export class AppModule {
 
